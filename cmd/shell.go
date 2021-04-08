a6c52a06b75e7f8a21ced2d3e744ee65b94125a2
Danny August Ramaputra <daystram@gmail.com>
2021-04-08 18:35:24 +1000

    feat: added interactive shell

---
+++ cmd/shell.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/daystram/quadsql/handlers"
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/cobra"
+)
+
+func interactive(cmd *cobra.Command, args []string) {
+	var err error
+	var result string
+	prompt := promptui.Prompt{
+		Label: ">",
+		Templates: &promptui.PromptTemplates{
+			Prompt:  "{{ . }} ",
+			Valid:   "{{ . | green }} ",
+			Invalid: "{{ . | red }} ",
+			Success: "{{ . | gray }} ",
+		},
+	}
+
+	for err == nil {
+		if result, err = prompt.Run(); err != nil {
+			break
+		}
+		err = handlers.HandleQuery(result)
+	}
+
+	switch err {
+	case nil, promptui.ErrInterrupt:
+		os.Exit(0)
+	default:
+		fmt.Printf("an error has occurred. %v\n", err)
+		os.Exit(1)
+	}
+}
---
+++ handlers/query.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"fmt"
+
+	"github.com/manifoldco/promptui"
+)
+
+func HandleQuery(query string) (err error) {
+	switch query {
+	case "/exit":
+		err = promptui.ErrInterrupt
+	default:
+		err = performQuery(query)
+	}
+	return
+}
+
+func performQuery(query string) (err error) {
+	fmt.Printf("QUERY: %s\n", query)
+	return
+}
