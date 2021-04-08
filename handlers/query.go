5f432095d800558d8c29c6338e2f8d584675a349
Danny August Ramaputra <daystram@gmail.com>
2021-04-08 22:34:36 +1000

    feat: updated handler vars

--- handlers/query.go
+++ handlers/query.go
@@ -5,20 +5,62 @@ import (
 
 	"github.com/manifoldco/promptui"
 
+	"github.com/daystram/quadsql/data"
 	"github.com/daystram/quadsql/db"
 )
 
-func HandleQuery(db *db.DB, query string) (err error) {
+type QueryConfig struct {
+	ShowTime   bool
+	UseIndex   bool
+	IndexReady bool
+}
+
+type Handler struct {
+	database *db.DB
+	config   *QueryConfig
+	index    data.QuadNode
+}
+
+func InitHandlers(database *db.DB, config *QueryConfig) Handler {
+	return Handler{
+		database: database,
+		config:   config,
+	}
+}
+
+func (h *Handler) BuildIndex() (err error) {
+	fmt.Printf("Building index... ")
+	// TODO: build index
+	h.index = data.QuadNode{}
+	h.config.IndexReady = true
+	fmt.Println("Done")
+	return
+}
+
+func (h *Handler) HandleQuery(query string) (err error) {
 	switch query {
 	case "/exit":
 		err = promptui.ErrInterrupt
+	case "/index on":
+		if h.config.IndexReady {
+			h.config.UseIndex = true
+			fmt.Println("Index enabled")
+		} else {
+			fmt.Println("Index has not been initialized, use '/index rebuild'")
+		}
+	case "/index off":
+		h.config.UseIndex = false
+		fmt.Println("Index disabled")
+	case "/index rebuild":
+		err = h.BuildIndex()
 	default:
-		err = performQuery(query)
+		err = h.performQuery(query)
 	}
 	return
 }
 
-func performQuery(query string) (err error) {
-	fmt.Printf("QUERY: %s\n", query)
+func (h *Handler) performQuery(query string) (err error) {
+	fmt.Printf("QUERY: %s \n", query)
+
 	return
 }
