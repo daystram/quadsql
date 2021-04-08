2c3a07f644d2c2d52f0ac7eaab21a51ad9cf045b
Danny August Ramaputra <daystram@gmail.com>
2021-04-08 23:59:33 +1000

    feat: added time report, added info

--- handlers/query.go
+++ handlers/query.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/manifoldco/promptui"
 
@@ -16,9 +17,10 @@ type QueryConfig struct {
 }
 
 type Handler struct {
-	database *db.DB
-	config   *QueryConfig
-	index    data.QuadNode
+	database     *db.DB
+	config       *QueryConfig
+	index        *data.QuadNode
+	lastExecTime int64
 }
 
 func InitHandlers(database *db.DB, config *QueryConfig) Handler {
@@ -31,7 +33,28 @@ func InitHandlers(database *db.DB, config *QueryConfig) Handler {
 func (h *Handler) BuildIndex() (err error) {
 	fmt.Printf("Building index... ")
 	// TODO: build index
-	h.index = data.QuadNode{}
+	h.index = &data.QuadNode{
+		Point: data.Point{
+			Coordinate: []float32{1, 2},
+		},
+		Children: []*data.QuadNode{{
+			Point: data.Point{
+				Coordinate: []float32{1, 2},
+			},
+		}, {
+			Point: data.Point{
+				Coordinate: []float32{1, 2},
+			},
+		}, {
+			Point: data.Point{
+				Coordinate: []float32{1, 2},
+			},
+		}, {
+			Point: data.Point{
+				Coordinate: []float32{1, 2},
+			},
+		}},
+	}
 	h.config.IndexReady = true
 	fmt.Println("Done")
 	return
@@ -53,14 +76,53 @@ func (h *Handler) HandleQuery(query string) (err error) {
 		fmt.Println("Index disabled")
 	case "/index rebuild":
 		err = h.BuildIndex()
+	case "/time":
+		if h.config.ShowTime {
+			h.config.ShowTime = false
+			fmt.Println("Time report disabled")
+		} else {
+			h.config.ShowTime = true
+			fmt.Println("Time report enabled")
+		}
+	case "/info":
+		count, depth := countNodes(h.index)
+		fmt.Printf("Dimension       : %d\n", h.database.Dimension)
+		fmt.Printf("DB Row Count    : %d\n", h.database.RowCount)
+		fmt.Printf("Index Enabled   : %s\n", map[bool]string{true: "Yes", false: "No"}[h.config.UseIndex])
+		fmt.Printf("Index Ready     : %s\n", map[bool]string{true: "Yes", false: "No"}[h.config.IndexReady])
+		fmt.Printf("Index Nodes     : %d nodes\n", count)
+		fmt.Printf("Index Max Depth : %d\n", depth)
+		fmt.Printf("Last Exec Time  : %d ms\n", h.lastExecTime/1000)
 	default:
 		err = h.performQuery(query)
+		if h.config.ShowTime {
+			fmt.Printf("Exec time: %d ms\n", h.lastExecTime/1000)
+		}
+
 	}
 	return
 }
 
 func (h *Handler) performQuery(query string) (err error) {
+	start := time.Now()
 	fmt.Printf("QUERY: %s \n", query)
+	// TODO
+	fmt.Printf("%+v\n", h.index)
+	h.lastExecTime = time.Since(start).Nanoseconds()
+	return
+}
 
+func countNodes(node *data.QuadNode) (count, depth int) {
+	if node != nil {
+		for _, child := range node.Children {
+			c, d := countNodes(child)
+			count += c
+			if d > depth {
+				depth = d
+			}
+		}
+		count++
+		depth++
+	}
 	return
 }
