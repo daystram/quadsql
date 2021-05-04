fa6309384586c8654b441b67412b0fcbb0bf6033
Danny August Ramaputra <daystram@gmail.com>
2021-05-04 19:31:25 +1000

    feat: added dataset and svg generator script

---
+++ generate-all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SEED=7
+SIZE=500
+
+mkdir -p datasets
+mkdir -p svg
+
+for DISTRIBUTION in "uniform" "normal" "line" "line-strict" "exp"; do
+    for SORTED_P in "sorted,true" "random,false"; do
+        IFS="," read SORTED SORTED_F <<< "$SORTED_P"
+        DB="datasets/$DISTRIBUTION-$SORTED.db"
+        echo $DB
+        ./quadsql --db $DB generate 2 $SIZE $DISTRIBUTION $SORTED_F --seed $SEED > /dev/null 2> /dev/null
+        echo "/svg 0.75 $DISTRIBUTION-$SORTED-point" | ./quadsql --db $DB > /dev/null 2> /dev/null
+        echo "/svg 0.75 $DISTRIBUTION-$SORTED-region" | ./quadsql --db $DB --region > /dev/null 2> /dev/null
+    done
+done
+
+mv *.svg svg/
