60865adf3797928d1cadfcc76251439ddb7faf50
Danny August Ramaputra <daystram@gmail.com>
2021-05-05 21:44:39 +1000

    feat: implemented index queries

--- handlers/query.go
+++ handlers/query.go
@@ -16,6 +16,13 @@ import (
 		// INSERT Point(5,6)
 */
 
+type QueryConfig struct {
+	ShowStat    bool
+	UseIndex    bool
+	IndexReady  bool
+	IsPointQuad bool // true: Point; false: Region
+}
+
 type QueryModel struct {
 	Type      string // SELECT, DELETE, INSERT
 	Condition Condition
@@ -37,19 +44,19 @@ func (h *Handler) performQuery(query string) (err error) {
 
 	// TODO: query parser
 
-	queryModel := QueryModel{
-		Type:      "SELECT",
-		Condition: Condition{},
-	}
 	// queryModel := QueryModel{
-	// 	Type: "SELECT",
-	// 	Condition: Condition{
-	// 		Field: "position",
-	// 		Value: data.Point{
-	// 			Position: []float64{278.99222549719235, 159.8223863234124},
-	// 		},
-	// 	},
+	// 	Type:      "SELECT",
+	// 	Condition: Condition{},
 	// }
+	queryModel := QueryModel{
+		Type: "SELECT",
+		Condition: Condition{
+			Field: "position",
+			Value: data.Point{
+				Position: []float64{220.15633674827117, 634.219107312793},
+			},
+		},
+	}
 	fmt.Printf("QUERY: %+v \n", queryModel)
 
 	start := time.Now()
@@ -57,7 +64,7 @@ func (h *Handler) performQuery(query string) (err error) {
 	if result, err = h.execute(queryModel); err != nil {
 		return
 	}
-	h.lastExecTime = float64(time.Since(start).Nanoseconds())
+	h.statistic.TimeExec = float64(time.Since(start).Nanoseconds())
 
 	fmt.Println("\n  id\t position")
 	fmt.Println("----------------------------")
@@ -77,26 +84,51 @@ func (h *Handler) execute(query QueryModel) (result QueryResult, err error) {
 				result = append(result, Row{id, point})
 			}
 		} else {
+			accessIndex, accessTable, comparePoint := 0, 0, 0
 			switch query.Condition.Field {
 			case "id":
-				// linear scan on table
+				// direct table access
 				id := query.Condition.Value.(int)
 				if id < len(h.database.Table) {
+					accessTable++
 					result = append(result, Row{id, h.database.Table[id]})
 				}
 			case "position":
 				position := query.Condition.Value.(data.Point)
 				if h.config.UseIndex {
 					if h.config.IsPointQuad {
-						// TODO: point query using Point index on table
-						break
+						// point query using Point index on table
+						node := h.index
+						for node != nil {
+							accessIndex++
+							comparePoint++
+							if node.Centre.CompareTo(position) == 0 {
+								accessTable++
+								result = append(result, Row{*node.PointID, h.database.Table[*node.PointID]})
+								break
+							} else {
+								node = node.Children[getQuadrant(node.Centre, position)]
+							}
+						}
 					} else {
-						// TODO: point query using Region index on table
-						break
+						// point query using Region index on table
+						accessIndex++
+						node := h.index
+						for node.PointID == nil {
+							accessIndex++
+							node = node.Children[getQuadrant(node.Centre, position)]
+						}
+						comparePoint++
+						if node.Centre.CompareTo(position) == 0 {
+							accessTable++
+							result = append(result, Row{*node.PointID, h.database.Table[*node.PointID]})
+						}
 					}
 				} else {
 					// linear scan on table
 					for id, point := range h.database.Table {
+						comparePoint++
+						accessTable++
 						if point.CompareTo(position) == 0 {
 							result = append(result, Row{id, point})
 							break
@@ -104,11 +136,10 @@ func (h *Handler) execute(query QueryModel) (result QueryResult, err error) {
 					}
 				}
 			}
+			fmt.Printf("Index accesses  : %d\n", accessIndex)
+			fmt.Printf("Table accesses  : %d\n", accessTable)
+			fmt.Printf("Point comparison: %d\n", comparePoint)
 		}
-	case "DELETE":
-		break
-	case "INSERT":
-		break
 	default:
 		err = ErrInvalidQuery
 	}
